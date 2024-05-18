5a306a6175aa67ff476405dfbcf2252a075cf834
dutra1243 <facundodutraextra@gmail.com>
2024-05-18 01:46:38 -0300

    inicio proyecto

---
+++ obligatorio/src/main/java/com/obligatorio/App.java
@@ -0,0 +1,13 @@
+package com.obligatorio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
